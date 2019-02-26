f0bfda2c785e70fdaec1d60bc6729e2ee2f423a9
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2019-02-26 01:17:53 +0100

    feat: rewrite module source code for v0.12
    Run `terraform 0.12upgrade` in the repo.
    
    BREAKING CHANGE: module has been upgraded to use Terraform v0.12-only features.

--- main.tf
+++ main.tf
@@ -1,35 +1,36 @@
 data "github_team" "parent" {
-  count = "${var.parent_team != "" ? 1 : 0}"
+  count = var.parent_team != "" ? 1 : 0
 
-  slug = "${var.parent_team}"
+  slug = var.parent_team
 }
 
 resource "github_team" "main" {
-  name = "${var.name}"
+  name = var.name
 
-  description = "${var.description}"
+  description = var.description
 
-  privacy = "${var.privacy}"
+  privacy = var.privacy
 
-  parent_team_id = "${element(concat(data.github_team.parent.*.id, list("0")), 0)}"
+  parent_team_id = element(concat(data.github_team.parent.*.id, ["0"]), 0)
 }
 
 resource "github_team_membership" "maintainers" {
-  count = "${length(var.maintainers)}"
+  count = length(var.maintainers)
 
-  team_id = "${github_team.main.id}"
+  team_id = github_team.main.id
 
-  username = "${element(var.maintainers, count.index)}"
+  username = element(var.maintainers, count.index)
 
   role = "maintainer"
 }
 
 resource "github_team_membership" "members" {
-  count = "${length(var.members)}"
+  count = length(var.members)
 
-  team_id = "${github_team.main.id}"
+  team_id = github_team.main.id
 
-  username = "${element(var.members, count.index)}"
+  username = element(var.members, count.index)
 
   role = "member"
 }
+
